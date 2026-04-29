15b6ad6e064d02c20c8f111c9098877b29424d89
code-arif <arifulislam6460@gmail.com>
2026-04-29 14:08:32 +0600

    fix: update Axios base URL to point to the correct backend service

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,11 +2,11 @@ import axios from 'axios'
 
 // Create an Axios instance with the Laravel backend URL
 const api = axios.create({
-  baseURL: 'https://zenvex-service-management-backend.test/api',
+  baseURL: "https://service.thewarriors.team/api",
   headers: {
-    'Content-Type': 'application/json',
-    'Accept': 'application/json',
-  }
-})
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+});
 
 export default api
